5d724a96679be657219feb0d63e80f599ede6df4
jerryliu20d <jerryliu20d@126.com>
2024-11-23 01:16:33 -0600

    Add some singer

---
+++ config/constants.js
@@ -0,0 +1,32 @@
+let config = {
+    Name: "",
+    
+    BiliLiveRoomID: "32497242",
+    
+    NetEaseMusicId: "",
+    QQMusicId: "",
+    Footer: "Copyright © 2022 秋葉",
+
+    Cursor: true,
+
+    LanguageCategories: ["国语", "英语", "粤语"],
+    RemarkCategories: ["抽象", "戏腔", "甜歌", "EMO"],
+
+    BannerTitle: "",
+    BannerContent: [
+        `奥菲利亚咻Ofelia`,
+     ],
+
+    CustomButtons: [
+        {
+            link: "https://pome.vip/h7o5e5rF",
+            name: "提问箱",
+            image: "/assets/icon/tapechat.png"
+        }       
+    ]
+}
+
+
+module.exports = {
+    config
+}
---
+++ scripts/converter.py
@@ -0,0 +1,19 @@
+import json
+import pandas as pd
+import numpy as np
+
+song_df = pd.read_excel('music.xlsx')
+song_df = song_df.replace({np.nan: None})
+song_list = []
+
+for index, row in song_df.iterrows():
+    song_data = {"index": index, "song_name": row[0], "artist": row[1], "language": row[2], "remarks": row[3], "initial": row[4], "sticky_top": row[5], "paid": row[6], "BVID": row[7]}
+    if row[5] == 1:
+        song_list.insert(0,song_data)
+    else:
+        song_list.append(song_data)
+    
+    
+
+with open("../public/music_list.json", 'w') as f:
+    f.write(json.dumps(song_list))
